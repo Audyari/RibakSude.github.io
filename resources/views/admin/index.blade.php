3fb6270b885a8cb5793bdcf25f5734f03c4a90c1
Audyari <audy123ari@gmail.com>
2023-11-21 13:28:09 +0700

    selasa-21/11/2023/jam 13.28

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.base')
+@section('content')
+<h1>ADMIN DASBOARD</h1>
+    
+@endsection
---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.base')
+@section('content')
+<h1>USER DASBOARD</h1>
+    
+@endsection
